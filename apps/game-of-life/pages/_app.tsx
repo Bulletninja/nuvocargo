9da694388d419a03ff7c1d198059f66f682c7206
Bulletninja <luis.linuxero@gmail.com>
2022-04-24 15:33:09 -0600

    Implement

--- apps/game-of-life/pages/_app.tsx
+++ apps/game-of-life/pages/_app.tsx
@@ -1,16 +1,24 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
-
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: '#333',
+      },
+    },
+  },
+});
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to game-of-life!</title>
       </Head>
-      <main className="app">
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
-      </main>
+      </ChakraProvider>
     </>
   );
 }
